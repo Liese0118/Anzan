d8fca64fe5adcfc70c6ad6f9e49f0b1d82874d67
Liese Uriu <liese.c.uriu@gmail.com>
2019-11-27 17:13:34 +0900

    Initial Commit

---
+++ Anzan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anzan
+//
+//  Created by Liese Uriu on 2019/11/27.
+//  Copyright © 2019 Liese Uriu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
